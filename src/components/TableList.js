af82c418d82736262f6a9e75a3f254762c17c1d4
InnaKovb <innaakovb@gmail.com>
2022-10-06 12:58:39 +0300

    fetch the request to back with popular movies

--- src/components/TableList.js
+++ src/components/TableList.js
@@ -1,9 +1,33 @@
 import EmptyMessage from "./EmptyMessage";
+import { useState ,  useEffect} from 'react';
 
 function TableList () {
     
+    const [movies, setMovies] = useState([]);
+    const API_KEY = 'bda7fa433b3a835662d4cd7b68962b54';
+    const apiURL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    
+
+    useEffect (() => {
+        fetch(apiURL)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setMovies(data.results);
+        })
+    }, [] )
+
     return (
-        <EmptyMessage/>
+        <>          
+            <EmptyMessage/>
+            <h2>Popular movies:</h2>
+            <ul>
+                {movies.map(movie => (<li key={movie.id}>{movie.original_title}</li>))}
+            </ul>
+        </>
+        
+        
+
     )
 }
 
