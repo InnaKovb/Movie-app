7f299eb329f5f35bf5a485d3f030fc72d0115021
InnaKovb <innaakovb@gmail.com>
2022-11-24 15:25:48 +0200

    redux toolkit + pagination

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -3,10 +3,15 @@ import './MovieCard.scss';
 
 const API_IMG = 'https://image.tmdb.org/t/p/w500/'
 
-export default function MovieCard({title, poster_path, vote_average, release_date, overview}) {
+export default function MovieCard({title, poster_path, backdrop_path, vote_average, release_date, overview}) {
     const poster  = {
         backgroundImage: `url(${API_IMG + poster_path})`
     }
+
+    const backdrop = {
+        backgroundImage: `url(${API_IMG + backdrop_path})`
+    }
+
     return (
         <div  className="card">
             <div className="card__front">
@@ -15,13 +20,20 @@ export default function MovieCard({title, poster_path, vote_average, release_dat
             <div className="card__back">
                 <div className="card__back-img" style={poster} >
                     <div className="card__content" >
-                        {/* <img className="card__img" src={API_IMG + poster_path} /> */}
                             <h1 className="card__title">{title}</h1>
-                        <h2 className="card__release">Release date: {release_date}</h2>
-                        <h3 className="card__rate">IMDb: {vote_average}</h3>
-                        <p className="card__overview">SUMMARY: {overview}</p>  
-                    </div>       
-                    
+                        <div className="card__rate">
+                            <span className='card__rate--title'>IMDb: </span>
+                            <span  className='card__rate--vote'>{vote_average}</span>
+                        </div>
+                        {/* <h3 className="card__release">Release Date: {release_date}</h3> */}
+                        <div className='card__overview-decor'>
+                            <p className="card__overview">{overview}</p>  
+                        </div>
+                        <div className='card__btns'>
+                            <a className="card__btn-first" >Watch Now</a>
+                            <a className="card__btn-second">Add to Watchlist</a>
+                        </div>       
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/TableList.js
+++ src/components/TableList.js
@@ -2,50 +2,56 @@ import EmptyMessage from "./EmptyMessage";
 import { useState ,  useEffect} from 'react';
 import Loader from "./Loader/Loader";
 import MovieCard from "./MovieCard/MovieCard";
-
+import Pagination from "./Pagination/Pagination";
+import { requestMoviesList } from "../store/movies/actions";
+ import { useSelector, useDispatch } from 'react-redux';
+ import "./TableList.scss";
 
 
 function TableList () {
-    const wrapper = {
-        overflowX: 'auto',
-        overflowY: 'hidden',
-        whiteSpace: 'nowrap',
-        margin: '20px'
+    const dispatch = useDispatch();
+    const moviesData = useSelector((state) => state.moviesStore.moviesData);
+    const totalMoviesPages = useSelector((state) => state.moviesStore.totalMoviesPages);
+    const  isLoading = useSelector((state) => state.moviesStore.loading);
+
+    console.log(moviesData);
+    console.log(totalMoviesPages);
+    useEffect (() => {
+            dispatch(requestMoviesList())
+            
+        }, [] )
+
+    const moviesPageChange = (page) => { 
+            const selectedPage  = page.selected + 1;
+            dispatch(requestMoviesList(selectedPage))
+            console.log(page,  "test") 
+       
     }
 
-    const [movies, setMovies] = useState([]);
-    const API_KEY = 'bda7fa433b3a835662d4cd7b68962b54';
-    const apiURL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-
-    const [loading, setLoading] = useState(false);
-
-    
-
-    useEffect (() => {
-        setLoading(true)
-        fetch(apiURL)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            setMovies(data.results);
-            setLoading(false);
-        })
-    }, [] )
 
     return (
         <>          
-            <EmptyMessage/>
-            <Loader isLoading={loading} loaderColor='orange'/>
-            <h2>Popular movies:</h2>
-            {/* {if (movies.length } */}
-            {/* <ul>
-                {movies.map(movie => (<li key={movie.id}>{movie.original_title}</li>))}
-            </ul> */}
-            <div  style={wrapper}>
-                {movies.map(movie => 
-            <MovieCard key={movie.id} {...movie}/>)}
-            </div>
-            
+            {moviesData.lenght > 0 ? (
+            <Loader isLoading={isLoading} loaderColor='orange'/>,
+            <EmptyMessage/>) 
+            : (
+                <div className="wrapper">
+                    <div className="wrapper-overlay">
+                        <h2 className='title'>Popular Movies:</h2>
+                        <Loader isLoading={isLoading} loaderColor='orange'/>
+                        <div>
+                            <div className='movieCardInner'>
+                                {moviesData.map(movie => 
+                                <MovieCard key={movie.id} {...movie}/>)
+                                }
+                            </div>  
+                            <Pagination totalPages={500} handlePageChange={(data)=> moviesPageChange(data)}/>    
+                        </div>
+                                
+                    </div> 
+                </div>
+                
+            )}      
         </>
 
     )
